3531a62d3f49912215b53c4369b6d59d5e643460
Jessica Torres <tjessica750@gmail.com>
2022-04-26 22:18:43 -0500

    ejercicioGrains

---
+++ grains/grains.py
@@ -0,0 +1,6 @@
+def square(integer_number):
+    if not 1 <= integer_number <= 64:
+        raise ValueError('square must be between 1 and 64')
+    return 1 << (integer_number - 1)
+def total():
+    return (1 << 64) - 1
